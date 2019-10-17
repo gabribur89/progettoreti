c0a3b32fb8f11cb231e00cd580b3adc7128c84b6
gabriwin <burattogabriele@hotmail.com>
2019-10-17 16:28:06 +0200

    altri files

---
+++ prova.js
@@ -0,0 +1,64 @@
+var http = require("http");
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var queue = 'hello';
+        var msg = res.send(reply);
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+        channel.sendToQueue(queue, Buffer.from(msg));
+
+        console.log(" [x] Sent %s", msg);
+    });
+    setTimeout(function() {
+        connection.close();
+        process.exit(0);
+    }, 500);
+});
+
+// Running Server Details.
+var server = app.listen(8082, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at %s:%s Port", host, port)
+});
+
+ 
+app.get('/form', function (req, res) {
+  var html='';
+  html +="<body>";
+  html += "<form action='/thank'  method='post' name='form1'>";
+  html += "Name:</p><input type= 'text' name='name'>";
+  html += "Email:</p><input type='text' name='email'>";
+  html += "address:</p><input type='text' name='address'>";
+  html += "Mobile number:</p><input type='text' name='mobilno'>";
+  html += "<input type='submit' value='submit'>";
+  html += "<INPUT type='reset'  value='reset'>";
+  html += "</form>";
+  html += "</body>";
+  res.send(html);
+});
+ 
+app.post('/thank', urlencodedParser, function (req, res){
+  var reply='';
+  reply += "Your name is" + req.body.name;
+  reply += "Your E-mail id is" + req.body.email; 
+  reply += "Your address is" + req.body.address;
+  reply += "Your mobile number is" + req.body.mobilno;
+  res.send(reply);
+ });
