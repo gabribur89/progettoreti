0581ed99d2847cb64b9cb054a17a8f9e5123171a
fburatta <fabrizio@moldiscovery.com>
2020-02-05 17:52:22 +0100

    aggiornamento SQL files

--- SQL/pg_function.sql
+++ SQL/pg_function.sql
@@ -3,17 +3,20 @@ DECLARE
   id bigint;
   nome varchar;
   cognome varchar;
+  dataiscrizione date;
 BEGIN
   IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' OR TG_OP = 'DELETE' THEN
     id = NEW.id;
     nome = NEW.nome;
     cognome = NEW.cognome;
+    dataiscrizione = NEW.dataiscrizione;
   ELSE
     id = OLD.id;
     nome = OLD.nome;
     cognome = OLD.cognome;
+    dataiscrizione = OLD.dataiscrizione;
   END IF;
-  PERFORM pg_notify('results', json_build_object('table', TG_TABLE_NAME, 'id', id, 'nome', nome, 'cognome', cognome, 'type', TG_OP)::text);
+  PERFORM pg_notify('results', json_build_object('table', TG_TABLE_NAME, 'id', id, 'nome', nome, 'cognome', cognome, 'dataiscrizione', dataiscrizione, 'type', TG_OP)::text);
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
